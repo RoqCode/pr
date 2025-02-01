70038b84aa0dbb5519d2ba84716427f3f68d9a99
Hajo Haas <hajo.haas@hmmh.de>
2025-02-01 09:26:09 +0100

    refactor

--- main.go
+++ main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
-func main() {
-	// get branch name
+func getBranchName() (string, error) {
 	branchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 	branchName, err := branchCmd.Output()
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return "", err
 	}
 
-	fmt.Println(string(branchName))
+	return string(branchName), nil
+}
 
-	// get remote url
+func getRemoteName() (string, error) {
 	remoteCmd := exec.Command("git", "config", "--get", "remote.origin.url")
 	remoteName, err := remoteCmd.Output()
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return "", err
+	}
+
+	return string(remoteName), nil
+}
+
+func main() {
+	branchName, err := getBranchName()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(string(branchName))
+
+	remoteName, err := getRemoteName()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	fmt.Println(string(remoteName))
