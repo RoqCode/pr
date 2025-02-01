5d3400dc85fa6bb0a97d90b35a9bec22ebf7a01f
Hajo Haas <hajo.haas@hmmh.de>
2025-02-01 09:15:23 +0100

    get branch and remote name

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-  // get branchname
-  // get remote address
-  // todo decide if github or bitbucket
-  // form url
-  // open url
+	// get branch name
+	branchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	branchName, err := branchCmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(branchName))
+
+	// get remote url
+	remoteCmd := exec.Command("git", "config", "--get", "remote.origin.url")
+	remoteName, err := remoteCmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(remoteName))
 }
